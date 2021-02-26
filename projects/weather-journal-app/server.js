2b80468dfc84d6051495e0fde4bfeab2b159b68b
AleOlea <espiritusdelfuego@gmail.com>
2021-02-26 17:27:30 -0300

    first commit

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,54 @@
 // Setup empty JS object to act as endpoint for all routes
+
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // Start up an instance of app
+const app = express(); //Ale commentary /this is an empty server.
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors for cross origin allowance//My add the two next lines
+const cors = require('cors'); //Ale commentary for web API and rubric request
+app.use(cors());
+
+const { request, response } = require("express"); //??my add?
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static('website')); /*file that points the server to the index.html*/
 
 // Setup Server
+const port = 8000;
+app.listen(port);
+console.log("hello world");
+
+//MY add Call back to debug
+function listening() {
+    console.log("server running");
+    console.log(`running on localhost: ${port}`);
+}
+
+// GET ROUTE.
+
+app.get("/all", function(req, res) {
+    res.send(projectData);
+});
+
+//MY ADD POST route add data to the project
+
+app.post("/add", function(req, res) {
+
+        projectData = {
+            temperature: `temp: ${req.body.temperature}°F`,
+            date: `date: ${req.body.date}`,
+            userFeelings: `userFeelings ${req.body.userResponse}`,
+        }
+        res.sendStatus(200)
+    })
+    //we are just sending a status not data is not a get request a post request.
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,89 @@
 /* Global Variables */
 
+
+
+//Personal API/ALE my add------
+const appID = "9ad2ba6aeb51853a32d2a00a33ecb79d";
+const baseURL = "http://api.openweathermap.org/data/2.5/weather";
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
+
+//MY ADD  //Create event listener
+document.getElementById("generate").addEventListener("click", generateEntry);
+
+
+
+async function generateEntry() {
+    let zipCode = document.getElementById("zip").value;
+    let userFeelings = document.getElementById("feelings").value;
+    let URL = `${baseURL}?zip=${zipCode}&appid=${appID}&units=imperial`;
+    let weatherData = {}
+
+    (await fetch(URL)).json().then(data => {
+            console.log(data)
+            weatherData = {
+                temperature: data.main.temp,
+                date: newDate,
+                userFeelings: userFeelings,
+            }
+        })
+        (await fetch("/add", { method: "post", body: weatherData }))
+        (await fetch("/all")).json().then(projectData => {
+            document.getElementById("date").innerText = projectData.date;
+            document.getElementById("temp").innerText = projectData.temperature;
+            document.getElementById("content").innerText = projectData.userFeelings;
+
+        })
+
+}
+
+
+
+
+
+/* MY DRAFT
+
+async function generateEntry(e) {
+    let zipCode = document.getElementById("zip").value;
+    const userResponse = document.getElementById("feelings").value;
+
+    let weatherData = await getWeatherData(baseURL, zipCode, appID)
+    const postResponse = await postData("/add", {
+        temperature: weatherData.main.temp,
+        date: newDate,
+        userResponse: userResponse,
+    });
+    if (postResponse.status === 200) {
+        updateUI("/all");
+    }
+}
+
+// MY ADD
+
+const getWeatherData = async(baseURL, zipCode, appID) => {
+    const openWeatherApiUrl = baseURL + zipCode + "&appid=" + appID + "&units = imperial";
+    const response = await fetch(openWeatherApiUrl);
+    return await response.json();
+};
+
+// MY ADD
+const postData = async(url = "", data = {}) => {
+    const response = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    return await response.json();
+}*/
+
+
+
+
+
+/*import { response } from "express"; //app.js:Uncaught SyntaxError: Cannot use import statement outside a moduleno me acuerdo esta linea mirar en cod original*/
